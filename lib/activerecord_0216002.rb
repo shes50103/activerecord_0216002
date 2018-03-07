fdef0003b2ab76d3df273d3b81d8b554a5eaf81f
shes50103 <shes50103@gmail.com>
2018-03-07 15:26:38 +0800

    start

---
+++ lib/activerecord_0216002.rb
@@ -0,0 +1,5 @@
+require "activerecord_0216002/version"
+
+module Activerecord0216002
+  # Your code goes here...
+end
---
+++ spec/activerecord_0216002_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Activerecord0216002 do
+  it "has a version number" do
+    expect(Activerecord0216002::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
